58be49522ab8616fdee67bf900ee153cc673a35c
Tyler Gilmore <tylergilmore0@gmail.com>
2021-03-11 17:56:16 -0600

    Following the docs for the first plugin

--- src/test/java/com/first/FirstPluginTest.java
+++ src/test/java/com/first/FirstPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.first;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class FirstPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(FirstPlugin.class);
 		RuneLite.main(args);
 	}
 }
